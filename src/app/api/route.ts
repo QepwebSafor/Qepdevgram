b65328479df0db82f25365c9b37d3bc5eb945a9c
QepwebSafor <qepsaforweb@gmail.com>
2026-01-19 07:55:36 -0800

    validating  resend

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -27,7 +27,7 @@ export async function POST(request: NextRequest) {
     const ownerEmail = userData.user.email;
 
     const { data, error } = await resend.emails.send({
-      from: "Qepdev <no-reply@qepdev.com.es>",
+      from: "Quica <no-reply@qepdev.com.es>",
       to: [ownerEmail],
       subject: `💬 ${commenterUsername} comentó en tu post`,
       html: `
